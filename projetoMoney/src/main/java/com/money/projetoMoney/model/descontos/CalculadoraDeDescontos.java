cefff181680a2027f3cb45b5388d497a7b83d055
crispim-luiz <criospim.silva@zup.coom.br>
2022-08-09 18:32:42 -0300

    Chain of Responsibility

---
+++ projetoMoney/src/main/java/com/money/projetoMoney/model/descontos/CalculadoraDeDescontos.java
@@ -0,0 +1,2 @@
+package com.money.projetoMoney.model.descontos;public class CalculadoraDeDescontos {
+}
---
+++ projetoMoney/src/main/java/com/money/projetoMoney/model/descontos/SemDesconto.java
@@ -0,0 +1,19 @@
+package com.money.projetoMoney.model.descontos;
+
+import com.money.projetoMoney.model.Orcamento;
+
+import java.math.BigDecimal;
+
+public class DescontoPorValor extends Desconto{
+    //Contrutor da class Desconto
+    public DescontoPorValor(Desconto proximo) {
+        super(proximo);
+    }
+    public BigDecimal calcular(Orcamento orcamento){//aplica desconto
+       if (orcamento.getValorOrcamento().compareTo(new BigDecimal("500")) > 0) {
+            return orcamento.getValorOrcamento().multiply( new BigDecimal("0.1"));
+        } else {//senao aplica o próximo
+            return proximo.calcular(orcamento);
+        }
+    }
+}
